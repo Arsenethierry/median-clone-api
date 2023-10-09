c1a6834c555a1e5757f69e793808da2d90ce3839
Arsene <78961020+Arsenethierry@users.noreply.github.com>
2023-10-09 15:54:56 +0000

    changes

---
+++ client/my-app/app/(user)/company/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CompanyProfile() {
+    return (
+        <div className='text-green-600 text-xl'>
+            onbording new company goes here...it can also be a new company referred by an existing users and get some amount of money for that
+        </div>
+    );
+}
+
+export default CompanyProfile;
